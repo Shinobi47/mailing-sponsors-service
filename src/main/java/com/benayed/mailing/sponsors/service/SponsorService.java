aa0f5290a1ad48938dfc9a25eaf83805dbd1f516
Haytam BENAYED <H.B>
2021-02-22 21:50:59 +0100

    add forgotten SponsorService

---
+++ src/main/java/com/benayed/mailing/sponsors/service/SponsorService.java
@@ -0,0 +1,32 @@
+package com.benayed.mailing.sponsors.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import com.benayed.mailing.sponsors.dto.SponsorDto;
+import com.benayed.mailing.sponsors.repository.SponsorDBRepository;
+import com.benayed.mailing.sponsors.utils.DataMapper;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class SponsorService {
+	
+	private SponsorDBRepository sponsorDBRepository;
+	private DataMapper dataMapper;
+	
+	public List<SponsorDto> fetchSponsors(){
+		log.info("Fetching sponsors ...");
+		
+		return this.sponsorDBRepository
+				.findAll().stream()
+				.map(dataMapper::toDto)
+				.collect(Collectors.toList());
+	}
+
+}
